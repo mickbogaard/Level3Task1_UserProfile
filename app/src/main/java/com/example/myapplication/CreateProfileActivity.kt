3b16e7a4e4bbd0b90829041b47a2814164cbc977
Mick Bogaard <mick.bogaard@hva.nl>
2020-04-11 16:41:41 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/CreateProfileActivity.kt
@@ -0,0 +1,66 @@
+package com.example.myapplication
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_create_profile.*
+
+const val GALLERY_REQUEST_CODE = 100
+private var profileImageUri: Uri? = null
+
+
+class CreateProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_profile)
+        initViews()
+    }
+
+    private fun initViews() {
+        btnGallery.setOnClickListener { onGalleryClick() }
+        btnConfirm.setOnClickListener { onConfirmClick() }
+
+    }
+
+    private fun onGalleryClick() {
+        // Create an Intent with action as ACTION_PICK
+        val galleryIntent = Intent(Intent.ACTION_PICK)
+
+        // Sets the type as image/*. This ensures only components of type image are selected
+        galleryIntent.type = "image/*"
+
+        // Start the activity using the gallery intent
+        startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                GALLERY_REQUEST_CODE -> {
+                    profileImageUri = data?.data
+                    ivProfileImage.setImageURI(profileImageUri)
+                }
+            }
+        }
+    }
+    private fun onConfirmClick() {
+        val profile = Profile(
+            etFirstName.text.toString(),
+            etLastName.text.toString(),
+            etDescription.text.toString(),
+            profileImageUri
+        )
+
+        val profileActivityIntent = Intent(this, ProfileActivity::class.java)
+        profileActivityIntent.putExtra(ProfileActivity.PROFILE_EXTRA, profile)
+        startActivity(profileActivityIntent)
+    }
+
+
+
+
+}
